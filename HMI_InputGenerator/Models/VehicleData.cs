55d21bc2e6f10eb870c5e13779243ff3eff7f43d
alexhendrik <45019513+alexhendrik@users.noreply.github.com>
2020-04-13 01:00:07 -0400

    Add files via upload

---
+++ HMI_InputGenerator/Models/VehicleData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMI_InputGenerator.Models
+{
+    public class VehicleData
+    {
+        public double LandSpeedMph { get; set; }
+        public double HorizontalPosition { get; set; }
+        public double ForwardProximity { get; set; }
+    }
+}
